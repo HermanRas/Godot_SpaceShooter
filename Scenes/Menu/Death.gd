0708200c4bec89b712ca02961dcdeee0563f381c
herman.ras.it@gmail.com <herman.ras.it@gmail.com>
2021-09-05 17:10:24 +0200

    Added Local Score Feature

--- Scenes/Menu/Death.gd
+++ Scenes/Menu/Death.gd
@@ -2,8 +2,11 @@ extends Control
 
 var _nul = ""
 
+var SCORE = []
+
 func _ready():
 	$NinePatchRect/VBoxContainer/lb_score.text = 'SCORE: ' + str(Global.SCORE)
+	loadScore()
 
 func _on_playBtn_pressed():
 	Global.SCORE = 0
@@ -16,3 +19,36 @@ func _on_quitBtn_pressed():
 func _on_ShareBtn_pressed():
 	var _err
 	_err = OS.shell_open("https://wa.me/?text=Nice I scored '" + str(Global.SCORE) +"' on \n\r https://dragoon.co.za/Space")
+
+func saveScore(Score,PlayerName):
+	SCORE.append({"name":PlayerName,"score":Score})
+	var content = JSON.print(SCORE)
+	var file = File.new()
+	file.open("user://SpaceShooter_Scores.dat", File.WRITE)
+	file.store_string(content)
+	file.close()
+
+func _on_saveBtn_pressed():
+	var PlayerName = $NinePatchRect/VBoxContainer/TextEdit.text
+	saveScore(str(Global.SCORE),PlayerName)
+	Global.SCORE = 0
+	var _sceneState
+	_sceneState= get_tree().change_scene("res://Scenes/World.tscn")
+
+func loadScore():
+	var file2Check = File.new()
+	var doFileExists = file2Check.file_exists("user://SpaceShooter_Scores.dat")
+	
+	if doFileExists :
+		var file = File.new()
+		file.open("user://SpaceShooter_Scores.dat", File.READ)
+		var content = JSON.parse(file.get_as_text())
+		file.close()
+		SCORE = content.result
+		SCORE.sort_custom(MyCustomSorter, "sort")
+
+class MyCustomSorter:
+	static func sort(a, b):
+		if a["score"] > b["score"]:
+			return true
+		return false
--- Scenes/Menu/MainMenu.gd
+++ Scenes/Menu/MainMenu.gd
@@ -1,9 +1,15 @@
 extends Control
-	
+
+var SCORE = []
+
 func _ready():
 	var screen_size = OS.get_screen_size()
 	var window_size = OS.get_window_size()
 	OS.set_window_position(screen_size*0.5 - window_size*0.5)
+	loadScore()
+	print(SCORE)
+	if SCORE.size() > 0:
+		$NinePatchRect/VBoxContainer/score_Name.text = SCORE[0].name + " " + SCORE[0].score
 
 func _on_playBtn_pressed():
 	var _sceneState
@@ -19,3 +25,21 @@ func _on_quitBtn_pressed():
 func _on_ShareBtn_pressed():
 	var _err
 	_err = OS.shell_open('https://dragoon.co.za/Space')
+
+func loadScore():
+	var file2Check = File.new()
+	var doFileExists = file2Check.file_exists("user://SpaceShooter_Scores.dat")
+	
+	if doFileExists :
+		var file = File.new()
+		file.open("user://SpaceShooter_Scores.dat", File.READ)
+		var content = JSON.parse(file.get_as_text())
+		file.close()
+		SCORE = content.result
+		SCORE.sort_custom(MyCustomSorter, "sort")
+
+class MyCustomSorter:
+	static func sort(a, b):
+		if a["score"] > b["score"]:
+			return true
+		return false
